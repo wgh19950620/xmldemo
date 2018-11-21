088619dd53600eca93728bca82cac455ceb0b6ea
wangguanghui <whui_wang@163.com>
2018-11-21 14:10:14 +0800

    post提交url携带参数传递测试

--- web/src/main/java/com/imooc/web/controller/UserController.java
+++ web/src/main/java/com/imooc/web/controller/UserController.java
@@ -5,6 +5,8 @@ import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.pagehelper.Page;
@@ -40,5 +42,29 @@ public class UserController {
         Page<User> page = userServiceImpl.findByPage(pageNo, pageSize);
         RestResponse response = RestResponse.success(new PageInfo<>(page));
         return response;
+    }
+
+    /**
+     * 测试post提交url拼接参数传递
+     *
+     * @param message 测试数据
+     */
+    @ApiOperation(value = "url测试", notes = "url测试")
+    @ApiImplicitParam(name = "message", value = "测试数据", required = true, dataType = "String", paramType = "query")
+    @PostMapping("/url/test")
+    public void urlRequestParamTest(String message) {
+        System.out.println(message);
+    }
+
+    /**
+     * 测试post提交url拼接参数传递
+     *
+     * @param message 测试数据
+     */
+    @ApiOperation(value = "url测试", notes = "url测试")
+    @ApiImplicitParam(name = "message", value = "测试数据", required = true, dataType = "String", paramType = "path")
+    @PostMapping("/url/test/{message}")
+    public void urlPathVariableTest(@PathVariable String message) {
+        System.out.println(message);
     }
 }
